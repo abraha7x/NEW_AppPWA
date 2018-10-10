047be0fdee1e867289c406ff86a2c00a7b2c2eed
abraha7x <miten.mitsurugi@gmail.com>
2018-10-10 00:22:16 -0500

    final commit

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+if ('serviceWorker' in navigator) {
+    console.log("Serviceworker activados");
+
+    navigator.serviceWorker.register('./sw.js')
+        .then(res => console.log('serviceworker cargado', res))
+        .catch(err => console.log('serviceworker no se ha podido registrar', err));
+
+} else {
+    console.log("No estan activados los serviceworker");
+}
+
 $(document).ready(function() {
     $("#menu a").click(function(e) {
         e.preventDefault();
